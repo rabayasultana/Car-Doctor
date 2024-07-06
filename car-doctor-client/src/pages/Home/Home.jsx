f6a7fa4e7c9db77f8049b8beda4e4dc40dd479d3
RabayaSultana <rabayashova811@gmail.com>
2024-07-06 16:58:23 +0600

    first commit

---
+++ car-doctor-client/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import About from "./About";
+import Banner from "./Banner";
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This is home</h2>
+            <Banner></Banner>
+            <About></About>
+        </div>
+    );
+};
+
+export default Home;
